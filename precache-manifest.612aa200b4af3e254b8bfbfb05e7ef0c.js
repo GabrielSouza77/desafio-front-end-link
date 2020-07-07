d9e593dfe0df11bdf294a7236f4bdfd4497b8a01
Gabriel dos Santos Souza <souzagabriels@hotmail.com>
2020-07-07 12:44:31 -0300

    Updates

---
+++ precache-manifest.612aa200b4af3e254b8bfbfb05e7ef0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9951dbf2f81aa33f80846add98f6d115",
+    "url": "/desafio-front-end/index.html"
+  },
+  {
+    "revision": "1eb08338221cbcaf54e3",
+    "url": "/desafio-front-end/static/css/main.f64f579d.chunk.css"
+  },
+  {
+    "revision": "0d75e30592743e2c0e8b",
+    "url": "/desafio-front-end/static/js/2.f0295de0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/desafio-front-end/static/js/2.f0295de0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb08338221cbcaf54e3",
+    "url": "/desafio-front-end/static/js/main.ac15a874.chunk.js"
+  },
+  {
+    "revision": "b94090d01755ba730dbd",
+    "url": "/desafio-front-end/static/js/runtime-main.f6fee991.js"
+  }
+]);
